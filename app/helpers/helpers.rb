91c38fbea117e1b1c0f002e05aead5f824474c0d
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-30 00:39:41 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,7 +7,7 @@ class Helpers
         @user = User.find_by_id(session[:user_id])
     end
 
-    def self.is_logged_in?
+    def self.is_logged_in?(session)
         !!session[:user_id] # !! to be sure that the method is returning only true or false
     end
 
