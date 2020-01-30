4e9dd5280ce2209dd52bd688c4d563ad5d06293c
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-30 00:40:59 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,7 @@
 #Instead of writing that type of logic directly into a view, we use helper methods. 
 #Helper methods are methods that are written in the controller, are accessible in the views, and provide some logical support. 
 #These two methods will only ever be called in views
-class Helpers < ActivRecord::Base
+class Helpers < ActiveRecord::Base
     def self.current_user(session)
         @user = User.find_by_id(session[:user_id])
     end
