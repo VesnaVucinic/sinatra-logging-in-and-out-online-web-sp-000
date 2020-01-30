d45b8206e3f0426c2d1ecdccf071ed6b7bad9226
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-30 00:38:02 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 class User < ActiveRecord::Base
     validates_presence_of :username, :password, :balance
 end
+=======
+class User < ActivRecord::Base 
+end
+
+>>>>>>> 1e2ca8c1ab7f9a1f451a34fa03a98183a0acf31e
